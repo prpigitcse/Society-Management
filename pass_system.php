609ee8e744b3cdd2f1686930f748f4fbe626384d
anushspecbee <42442042+anushspecbee@users.noreply.github.com>
2018-11-16 17:31:24 +0530

    add files

---
+++ pass_system.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>specbee</title>
+    <link rel="shortcut icon" type="image/png" href="images/favicon.ico"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/index.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+<div class="wrapper fadeInDown">
+  <div id="formContent">
+    <!-- Tabs Titles -->
+    <h2 class="inactive underlineHover"> <a href="dashboard.php">submit isuue  </h2>
+    <h2 class="inactive underlineHover"> <a href="search issue.php"> check your issue </a></h2>
+    <h2 class="active"> Pass System </a></h2>
+
+
+    <!-- Icon -->
+
+
+    <!-- Login Form -->
+    <form method="POST" action="search issue.php">
+    <input type="text" id="subject" class="fadeIn second"  required="required" name="pass_name" placeholder="Name">
+    <input type="text" id="subject" class="fadeIn second"  required="required" name="pass_name" placeholder="Pass Id">
+    <input type="text" id="subject" class="fadeIn second"  required="required" name="pass_name" placeholder="address">
+    <input type="text" id="subject" class="fadeIn second"  required="required" name="pass_name" placeholder="Romms required">
+    <input type="text" id="subject" class="fadeIn second"  required="required" name="pass_name" placeholder="Number of Peoples">
+    <input type="text" id="subject" class="fadeIn second"  required="required" name="pass_name" placeholder="specify extra">
+
+      <input type="submit" class="fadeIn fourth" value="submit" name="submit">
+    </form>
+
+    <!-- Remind Passowrd -->
+    <div id="formFooter">
+ <b> Society Management services </b><br>
+ <a href="logout.php"><input type="button" value="Logout"></a>
+
+    </div>
+
+  </div>
+</div>
+</body>
+</html>
+
+
